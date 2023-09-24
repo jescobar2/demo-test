943b1e87c3aa05729fe8ef5006b66ca32ac8fb44
jescobar <jescobar@questrade.com>
2023-09-23 19:42:30 -0500

    changing name

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask
 import os
+import pika
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     host = os.uname()[1]
+    # Configuración de la conexión a RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_HOST']))
+    channel = connection.channel()
+
+    # Declarar una cola
+    channel.queue_declare(queue='mi_cola')
+
+    # Mensaje que deseas enviar
+    mensaje = "Hola, RabbitMQ!"
+
+    # Publicar el mensaje en la cola
+    channel.basic_publish(exchange='', routing_key='mi_cola', body=mensaje)
+
+    print(f"Mensaje enviado: '{mensaje}'")
+
+    # Cerrar la conexión
+    connection.close()
     return f"Hello, fucking world!\nVersion: 1.0.0\nHostname: {host} and {os.environ['RABBITMQ_HOST']}\n"
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', '8080')
     app.run(host='0.0.0.0', port=int(port))
+
