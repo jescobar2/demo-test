9aa375476c6b280e44679715c2c5b55ad583f57b
jescobar <jescobar@questrade.com>
2023-09-26 17:49:21 -0500

    changing name

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ from typing import Callable
 from concurrent import futures
 from google.cloud import pubsub_v1
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     host = os.uname()[1]
+@app.route('/')
+def hello():
+    host = os.uname()[1]
     # Configuración de la conexión a RabbitMQ
     # try:
     #     connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_HOST']))
@@ -59,11 +59,11 @@ from google.cloud import pubsub_v1
     # except Exception as e:
     #     a = "no publico"
 
-#    return f"Hello, publisher fucking world!\nVersion: 1.0.0\nHostname: {host} and {os.environ['RABBITMQ_HOST']}\n"
+    return f"Hello, publisher fucking world!\nVersion: 1.0.0\nHostname: {host} and {os.environ['RABBITMQ_HOST']}\n"
 
-# if __name__ == '__main__':
-#     port = os.environ.get('PORT', '8080')
-#     app.run(host='0.0.0.0', port=int(port))
+if __name__ == '__main__':
+    port = os.environ.get('PORT', '8080')
+    app.run(host='0.0.0.0', port=int(port))
 
 
 # def main():
@@ -110,13 +110,13 @@ from google.cloud import pubsub_v1
 # if __name__ == '__main__':
 #     main()
 
-import json
-from flask import Flask, request
+# import json
+# from flask import Flask, request
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # Configuración de Google Cloud Pub/Sub
-from google.cloud import pubsub_v1
+# from google.cloud import pubsub_v1
 
 # def get_callback(
 #     publish_future: pubsub_v1.publisher.futures.Future, data: str
@@ -129,38 +129,38 @@ from google.cloud import pubsub_v1
 
 #     return callback
 
-@app.route('/')
-def publish_message():
-    try:
-        project_id = "hazel-champion-399821"
-        topic_id = "traslado-operador"
-        publisher = pubsub_v1.PublisherClient()
-        topic_path = publisher.topic_path(project_id, topic_id)
-        message = {"dataFormat": "JSON_API_V1", 
-                "eventType": "SECRET_ROTATE", 
-                "secretId": "solictando traslado a operador z",
-                "project_id": "hazel-champion-399821"}
-        # message_data = request.get_json()
-        data = json.dumps(message)
+# @app.route('/')
+# def publish_message():
+#     try:
+#         project_id = "hazel-champion-399821"
+#         topic_id = "traslado-operador"
+#         publisher = pubsub_v1.PublisherClient()
+#         topic_path = publisher.topic_path(project_id, topic_id)
+#         message = {"dataFormat": "JSON_API_V1", 
+#                 "eventType": "SECRET_ROTATE", 
+#                 "secretId": "solictando traslado a operador z",
+#                 "project_id": "hazel-champion-399821"}
+#         # message_data = request.get_json()
+#         data = json.dumps(message)
 
-        # Publicar el mensaje en Google Cloud Pub/Sub
-        publish_future = publisher.publish(topic_path, data.encode("utf-8"))
-        print(f"Published message: {publish_future.result()}")
+#         # Publicar el mensaje en Google Cloud Pub/Sub
+#         publish_future = publisher.publish(topic_path, data.encode("utf-8"))
+#         print(f"Published message: {publish_future.result()}")
 
-        # publish_future.add_done_callback(get_callback(publish_future, data))
-        # publish_futures = publish_future
+#         # publish_future.add_done_callback(get_callback(publish_future, data))
+#         # publish_futures = publish_future
 
-        # # Wait for all the publish futures to resolve before exiting.
-        # futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
+#         # # Wait for all the publish futures to resolve before exiting.
+#         # futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
    
-        print(f"Published messages with error handler to {topic_path}.")
-        return "Message published successfully!", 200
-    except Exception as e:
-        return str(e), 500
+#         print(f"Published messages with error handler to {topic_path}.")
+#         return "Message published successfully!", 200
+#     except Exception as e:
+#         return str(e), 500
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080)
 
 
 # from flask import Flask, request, jsonify, make_response
